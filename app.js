1431132f11012f85d521d7c110306f0f8a5cf6d9
Abdullah Itani <a.itani@live.ca>
2023-05-12 01:16:24 -0400

    finished front end

--- app.js
+++ app.js
@@ -14,25 +14,41 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
+    //geocode api used to find coordinates (latitude and longitude) of the user's input location
     const LocationAPI = `https://geocode.maps.co/search?street=${req.body.street}&city=${req.body.city}&state=${req.body.state}&postalcode=${req.body.postalCode}&country=${req.body.country}`;
 
-    async function getTemperature()
+    async function getWeatherInfo()
     {
         try{
+            //using geocode api, we get the lattitude and longitude of the location entered by the user 
             const locationAPIReq = await axios.get(LocationAPI);
             const lat = locationAPIReq.data[0].lat;
             const lon = locationAPIReq.data[0].lon;
             
+            //using open weather map api, we get the weather information of the location entered by the user
             const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiAcessToken}&units=metric`;
             const weatherAPIReq = await axios.get(weatherAPI);
-            const temperature = weatherAPIReq.data.main.temp;
-            res.render('result', {temp: temperature});
+
+            //the data that we want it to show up on the user's result page
+            const main = weatherAPIReq.data.main;
+            const weather = weatherAPIReq.data.weather[0];
+            const name = weatherAPIReq.data.name;
+            const wind_speed = weatherAPIReq.data.wind.speed;
+            const coord = weatherAPIReq.data.coord;
+            
+            //sending the data that we need to the user's page
+            res.render('result', {
+                main: main, 
+                weather: weather, 
+                name: name,
+                wind_speed: wind_speed,
+                coordinates: coord
+            });
         }catch(err){
-            console.log(`Error: ${err}`);
-            res.send(`Error: ${err}`)
+            res.render('error',{error: err});
         }
     }
-    getTemperature();
+    getWeatherInfo();
 });
 
 app.listen(3000);
